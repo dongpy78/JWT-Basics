3af4f71304700193968214139759b7802f28c55f
Van Dong <vandong78py@gmail.com>
2024-11-11 22:30:07 +0700

    Validate Username and Password:

--- controllers/main.js
+++ controllers/main.js
@@ -1,9 +1,25 @@
+// check username, password in post(login) request
+// if exist create new JWT token
+// send back to frontend
+
+// setup authentication so only the request with JWT can access the dashboard
+
 const login = async (req, res) => {
+  // check username, password in post(login) request
+  const { username, password } = req.body;
+
+  const CustomAPIError = require("../errors/custom-error");
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
   res.send("Fake Login/Register/Signup Route");
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.random() * 100;
+  const luckyNumber = Math.floor(Math.random() * 100);
+
   res.status(200).json({
     msg: `Hello, John Doe`,
     secret: `Here is your authorization data, your lucky number is ${luckyNumber}`,
